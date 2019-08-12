68d8d826b155b81518cd88498fa0f21b29d69b05
ch <ch@163.com>
2019-08-12 08:44:37 +0800

    111

---
+++ git-test/git_test/src/main/java/cn/itcast/test/Test1.java
@@ -0,0 +1,15 @@
+package cn.itcast.test;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+        System.out.println("aaaaa");
+        System.out.println("aaaaa");
+        System.out.println("aaaaa");
+        System.out.println("aaaaa");
+    }
+    public void test(){
+        int i = 1;
+        System.out.println(i+10);
+    }
+}
